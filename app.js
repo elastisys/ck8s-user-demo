00d57023711a854d7c512d6fff4cf6e01d98c39a
Cristian Klein <cristian.klein@elastisys.com>
2021-09-20 11:15:25 +0200

    Use structured logging

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ var crashRouter = require('./routes/crash');
 
 var app = express();
 
-app.use(logger('dev'));
+function structuredLogFormat(tokens, req, res) {
+  return JSON.stringify({
+    'remote-address': tokens['remote-addr'](req, res),
+    'time': tokens['date'](req, res, 'iso'),
+    'method': tokens['method'](req, res),
+    'url': tokens['url'](req, res),
+    'http-version': tokens['http-version'](req, res),
+    'status-code': tokens['status'](req, res),
+    'content-length': tokens['res'](req, res, 'content-length'),
+    'referrer': tokens['referrer'](req, res),
+    'user-agent': tokens['user-agent'](req, res),
+  });
+}
+
+app.use(logger(structuredLogFormat));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
